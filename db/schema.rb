cf33330738ee91ee9f5b51b5d4964af2665c0c69
Mario Jackson <mario@jackson.software>
2020-06-29 13:31:44 +0200

    added new fields to events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_29_090434) do
+ActiveRecord::Schema.define(version: 2020_06_29_111044) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2020_06_29_090434) do
     t.decimal "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "starts_at"
+    t.text "description"
   end
 
 end
