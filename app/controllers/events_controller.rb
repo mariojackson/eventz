335e5c471e112608fa5ada552d68cda220fa54f1
Mario Jackson <mario@jackson.software>
2020-06-29 22:45:58 +0200

    events edit route

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,4 +6,8 @@ class EventsController < ApplicationController
     def show
         @event = Event.find(params[:id])
     end
+
+    def edit
+        @event = Event.find(params[:id])
+    end
 end
