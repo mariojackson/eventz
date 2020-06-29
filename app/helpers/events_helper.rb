5e874e0717f38b692f1335b752902ee5e846d860
Mario Jackson <mario@jackson.software>
2020-06-29 14:07:27 +0200

    event helper for free events and human readable dates

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,13 @@
 module EventsHelper
+    def price(event)
+        if event.free?
+            "Free"
+        else
+            number_to_currency(event.price, precision: 0)
+        end
+    end
+
+    def day_and_time(event)
+        event.starts_at.strftime("%B %d at %I:%M %P")
+    end
 end
