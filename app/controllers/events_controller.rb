be18a8d670733005c8ec9901e393860ba67e47f5
Mario Jackson <mario@jackson.software>
2020-06-30 19:26:14 +0200

    made it able to patch an event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,13 +1,25 @@
+# frozen_string_literal: true
+
+# EventsController provides actions for the event resource
 class EventsController < ApplicationController
-    def index
-        @events = Event.all
-    end
+  def index
+    @events = Event.all
+  end
 
-    def show
-        @event = Event.find(params[:id])
-    end
+  def show
+    @event = Event.find(params[:id])
+  end
 
-    def edit
-        @event = Event.find(params[:id])
-    end
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    event_params = params.require(:event)
+                         .permit(:name, :description, :location, :price, :starts_at)
+    @event.update(event_params)
+
+    redirect_to @event
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
-Rails.application.routes.draw do
-  root "events#index"
+# frozen_string_literal: true
 
-  get "events" => "events#index"
-  get "events/:id" => "events#show", as: "event"
-  get "events/:id/edit" => "events#edit", as: "edit_event"
+Rails.application.routes.draw do
+  root 'events#index'
+
+  get 'events' => 'events#index'
+  get 'events/:id' => 'events#show', as: 'event'
+  get 'events/:id/edit' => 'events#edit', as: 'edit_event'
+
+  patch 'events/:id' => 'events#update'
 end
