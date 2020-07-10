ec06ca13ea4436e044bb00b5f99563bfa3b9840f
Mario Jackson <mario@jackson.software>
2020-07-10 14:06:37 +0200

    show registrations for an event

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,2 +1,8 @@
+# frozen_string_literal: true
+
 class RegistrationsController < ApplicationController
+  def index
+    @event = Event.find(params[:event_id])
+    @registrations = @event.registrations
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :registrations
-  root 'events#index'
+  resources :events do
+    resources :registrations
+  end
 
-  resources :events
+  root 'events#index'
 end
