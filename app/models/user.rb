a83db7656e46ea19a2d69c0cec155da1656f0ebc
Mario Jackson <mario@jackson.software>
2020-08-14 13:09:11 +0200

    many to many between users and registrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :registrations, dependent: :destroy
+
   has_secure_password
 
   validates :name, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_14_054300) do
+ActiveRecord::Schema.define(version: 2020_08_14_105930) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -25,12 +25,11 @@ ActiveRecord::Schema.define(version: 2020_08_14_054300) do
   end
 
   create_table "registrations", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
     t.string "how_heard"
     t.integer "event_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
     t.index ["event_id"], name: "index_registrations_on_event_id"
   end
 
